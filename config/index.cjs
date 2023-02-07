0ba4928beb943acd72057a555838b5077d182e2d
说于山鬼听 <58384034+WuJingTao-ai@users.noreply.github.com>
2023-02-07 11:45:01 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ofIGL5tErjRRoOE7ugZdxW3JmE3k',
+      id: 'ofIGL5kn6MQ28D5sd5CiY6-tVLt8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'IJvI8E1rAl4cpxX7sBIMbF43yED8RC30F83Ko473-j4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
